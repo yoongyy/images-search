c23d1414f7d7ac87ff0f75c3c9e94f79be4e1bf1
mkt-kenny-yew <mkt.kenny.yew@g-g-market.com>
2025-01-18 17:32:44 +0800

    Update app.js, .env_example, and docker-compose.yml with necessary changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
+// Connect MongoDB
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB connected'))
